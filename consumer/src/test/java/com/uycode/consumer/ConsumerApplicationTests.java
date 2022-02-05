6d729fa7cc1f24b1adaa1372959f5e2767d1bb4c
UyCode <Hyper-Hack@outlook.com>
2022-02-06 02:55:31 +0800

    add consumer

---
+++ consumer/src/test/java/com/uycode/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.uycode.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
